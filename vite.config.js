d7cb9077cf1a2aeddf3644f4b4fcce1dd56cb4d9
yusof_antar <youssof.antar@gmail.com>
2024-07-04 00:23:09 +0300

    Update

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "youssof-antar",
+      project: "apple-website",
+      release: {
+        name: "apple-website",
+      },
+      telemetry: false,
+    }),
+  ],
+
+  build: {
+    sourcemap: true,
+  },
+});
