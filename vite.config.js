6e0ed6759016eb066fdd2e4fddec51ae5b0eec14
yusof_antar <youssof.antar@gmail.com>
2024-07-04 09:26:54 +0300

    Updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/apple_website/",
+  base: "/",
   plugins: [
     react(),
     sentryVitePlugin({
